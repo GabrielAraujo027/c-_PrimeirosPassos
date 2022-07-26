bd3e08bdb2125db56ba799b6979f547c3f98197c
GabrielAraujo027 <gabrielaraujolira12@gmail.com>
2022-07-26 20:41:14 -0300

    Parte final dos primeiros passos

---
+++ Contador/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program 
+{
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("\nExecuntando Projeto teste - Contador \n");
+
+        // for (int contadorLinhas = 0; contadorLinhas < 100; contadorLinhas+=3)
+        // {
+        //     for(int contador = 0; contador < 100; contador+=3)
+        //     {
+        //         Console.Write(contador + "; ");
+        //         if(contador >= contadorLinhas)
+        //         {
+        //             break;
+        //         }
+        //     }
+        //     Console.WriteLine();
+        // }
+
+
+        int fatorial = 1;
+        for (int i = 1; i < 11; i++)
+        {
+            fatorial *= i;
+            Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+        }
+        
+        System.Console.WriteLine("\n Tecle enter para sair...");
+        Console.ReadLine();
+    }
+}
